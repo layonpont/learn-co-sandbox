1033cbfcbfdb48fd867e9cf7736fb1b2b8c454fa
dashing-page-6640 <elhadi.sulton@gmail.com>
2018-09-30 19:23:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,12 +13,12 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 # batch_badge_creator(names)
 
 def assign_rooms(speakers)
-  rooms = [1, 2, 3, 4, 5, 6, 7]
+  # rooms = [1, 2, 3, 4, 5, 6, 7]
   
-  speakers.each do |speaker|
-    rooms.each do |room|
+  speakers.each_with_index do |speaker|
+    
       puts "Hello #{speaker}! you are room #{room}"
-    end
+   
     
   end
 end 
