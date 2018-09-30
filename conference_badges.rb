706c6fec80c988b36d4017191bc420f298d558f8
dashing-page-6640 <elhadi.sulton@gmail.com>
2018-09-30 19:38:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,19 +1,21 @@
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 # Badge Maker Method
+
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end 
 
 # Batch Badge Creater Method
+
 def batch_badge_creator(names)
   names.each do |name|
     puts "Hello, my name is #{name}."
   end
 end
 
-names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-
 # Assign Rooms Method
+
 def assign_rooms(speakers)
   speakers.each_with_index do |speaker, room|
       room+=1
